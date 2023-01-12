67931f1e566ca7705eb2e4c6d127d39f94353fb7
Giorgio Perez <giorgiop62@gmail.com>
2023-01-12 18:14:02 +0100

    index finito

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <h1>FUMETTI</h1>
+    <div class="container">
+        <h1>FUMETTI</h1>
 
+        @forelse ($comics as $comic)
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="{{$comic->thumb}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $comic->title }}</h5>
+                    <p class="card-text">{{$comic->description}}</p>
 
-</div>
-
-
-@endsection
-
+                </div>
+            @empty
+        @endforelse
+    @endsection
