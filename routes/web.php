3acd3210e685b3a528966eaafdae03bfafbcaede
Giorgio Perez <giorgiop62@gmail.com>
2023-01-12 17:57:12 +0100

    index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicController::class, 'index'])->name('home');
+
+Route::resource('comics', ComicController::class);
